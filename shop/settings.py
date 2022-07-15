02ca845b946061be45074a89eb254bb99fa77454
Jamal130613 <liyapark.com@gmail.com>
2022-07-15 15:42:22 +0600

    new

--- shop/settings.py
+++ shop/settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'applications.account',
     'applications.card',
     'applications.product',
+
 ]
 
 MIDDLEWARE = [
@@ -133,3 +135,16 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 AUTH_USER_MODEL = 'account.CustomUser'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_PASSWORD = 'ycqpvcmokndwrqiw'
+EMAIL_HOST_USER = 'jamalaskarovaa@gmail.com'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTIFICATION_CLASSES':[
+        'rest_framework.authentification.TokenAuthentification'
+    ]
+}
